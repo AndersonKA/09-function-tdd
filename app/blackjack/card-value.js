459ebf61b3964d7cd8587b1fe90f3893fc5b7d73
Kae Anderson <andersonkaem@gmail.com>
2017-02-13 15:59:41 -0600

    did card values

--- app/blackjack/card-value.js
+++ app/blackjack/card-value.js
@@ -1,1 +1,25 @@
-export default function () {}
+export default function(card, value1, value2) {
+
+
+  if (card === '5') {
+    return 5;
+  }
+
+  if (card === 'jack') {
+    return 10;
+  }
+
+  if (card === 'queen') {
+    return 10;
+  }
+
+  if (card === 'king') {
+    return 10;
+  }
+
+  if (card === 'ace') {
+    return 11;
+  }
+
+  return parseInt(card);
+}
