3edaddd2ae146e305f6a1c204eeca572bb142f9b
Kae Anderson <andersonkaem@gmail.com>
2017-02-13 18:01:42 -0600

    started neighbors

--- app/conway/alive-neighbors.js
+++ app/conway/alive-neighbors.js
@@ -1,1 +1,10 @@
-export default function () {}
+export default function(arr) {
+  let alive = 0;
+
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i]) {
+      alive += 1;
+    }
+}
+    return alive;
+  }
